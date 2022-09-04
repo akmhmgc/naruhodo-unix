32e5ea4bda40848076bff45319fc8cfc7ad6b9d8
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-04 14:01:47 +0900

    Add pipe

--- ruby-magazine/pipe.rb
+++ ruby-magazine/pipe.rb
@@ -1,5 +1,14 @@
-read_io, write_io = IO.pipe
+IO.pipe do |read_io, write_io|
+  # 1つ目のコマンドを実行する子プロセス
+  fork do
+    STDOUT.reopen(write_io) # 標準出力をパイプにつけかえる
+    exec 'ps', '-x' # psコマンドを実行させる
+  end
 
-
-puts read_io.fileno
-puts write_io.fileno
+  # 2つ目のコマンドを実行する子プロセス
+  fork do
+    STDIN.reopen(read_io) # 標準入力をパイプにつけかえる
+    exec 'wc', '-l'
+  end
+end
+Process.waitall
